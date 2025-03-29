a5d15a231eb15f071fea147eb282fec7f79082ba
chinazok4 <141956701+chinazok4@users.noreply.github.com>
2025-03-28 20:08:48 -0400

    Update calc_response_limit.py

--- calc_response_limit.py
+++ calc_response_limit.py
@@ -1,1 +1,28 @@
+import requests
 
+access_token = 0
+
+API_URL = "https://router.huggingface.co/hf-inference/models/google/tapas-base-finetuned-wtq"
+headers = {"Authorization": "Bearer " + access_token}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": {
+	"query": "How many stars does the transformers repository have?",
+	"table": {
+		"Repository": ["Transformers", "Datasets", "Tokenizers"],
+		"Stars": ["36542", "4512", "3934"],
+		"Contributors": ["651", "77", "34"],
+		"Programming language": [
+			"Python",
+			"Python",
+			"Rust, Python and NodeJS"
+		]
+	}
+},
+})
+
+print output
